c7f30a1ecaae95804b3f25dde341df7fcf7ac22d
AbdulBasit-technoties <abasit.technotie@gmail.com>
2024-12-04 18:55:13 +0500

    update

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+class PostController extends Controller
+{
+    public function index() {
+        return Inertia::render('Posts/Index');
+    }
+}
